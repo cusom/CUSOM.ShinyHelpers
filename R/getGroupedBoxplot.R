2d9c40e08841b84b98263f3f1f819f4bb3652508
Bartsch <kyle.bartsch@cuanschutz.edu>
2020-10-02 10:16:53 -0600

    Adding getSideBySideGroupedBoxPlot, reverting getGroupedBoxPlot to version used in Transcriptome plots.

--- R/getGroupedBoxplot.R
+++ R/getGroupedBoxplot.R
@@ -1,10 +1,9 @@
-#' Plotly function to generate side-by-side box and whisker plots comparing groups by highlighted group
+#' Plotly function to generate multiple box and whisker plots comparing group across secondary grouping level
 #'
-#' @param .data dataframe containing numeric values between 2 groups
+#' @param .data dataframe containing numeric values between 2 groups, across secondary grouping level
 #' @param .key dataframe key column
 #' @param .group string - column indicating highlighted group membership (A vs B)
-#' @param .comparisonGroup string - column indicating the group label that should be used as the comparison
-#' @param .comparisonBaseline string - value indicating which of the comparison group values should be used as comparison
+#' @param .legendGroup string - column indicating the secondary group label
 #' @param .value numeric - column with numerical value to compare between groups
 #' @param .valueLabel string - column indicating the label for numerical values used in boxplots
 #' @param .text string - column containing text values to show in tooltip
@@ -15,20 +14,24 @@
 #' @export
 
 
-getGroupedBoxplot <- function(.data,.key,.group,.comparisonGroup,.comparisonBaseline,.value,.valueLabel,.text, showJitter = TRUE, plotName) {
-  # LabID,HighlightGroup,Analyte,input$kAnalyte,y,y_label,text,TRUE,"AnalyteComparison"
-  .key <- enquo(.key) #labId
-  .group <- enquo(.group) # highight group A vs B
-  .comparisonGroup <- enquo(.comparisonGroup) # Analyte
-  .comparisonBaseline <- enquo(.comparisonBaseline) # 1st analyte
-  .value <- enquo(.value) # measuredValue
-  .valueLabel <- enquo(.valueLabel) # concentration
+getGroupedBoxplot <- function(.data,.key,.group,.legendGroup,.value,.valueLabel,.text, showJitter = TRUE, plotName) {
+  # key = RecordID
+  # x = celltype
+  # y = MeasuredValue
+  # legendGroup = Karyotype
+  # color = Karytoye
+
+  .key <- enquo(.key)
+  .group <- enquo(.group)
+  .legendGroup <- enquo(.legendGroup)
+  .value <- enquo(.value)
+  .valueLabel <- enquo(.valueLabel)
   .text <- enquo(.text)
 
   if(nrow(.data)>0){
 
     .data <- .data %>%
-      mutate(key = !!.key, group = !!.group, comparisonGroup = !!.comparisonGroup, value = !!.value, text = !!.text)
+      mutate(key = !!.key, group = !!.group, legendGroup = !!.legendGroup, value = !!.value, text = !!.text)
 
     yVariableLabel <- .data %>%
       distinct(!!.valueLabel) %>%
@@ -64,93 +67,44 @@ getGroupedBoxplot <- function(.data,.key,.group,.comparisonGroup,.comparisonBase
                    t = 100,
                    b = 50)
 
-    d1 <- .data %>%
-      filter(comparisonGroup == !!.comparisonBaseline)
-
-    baselineLabel <- d1 %>%
-      select(comparisonGroup) %>%
-      unique() %>%
-      pull()
-
-    d2 <- .data %>%
-      filter(comparisonGroup != !!.comparisonBaseline)
-
-    comparisonLabel <- d2 %>%
-      select(comparisonGroup) %>%
-      unique() %>%
-      pull()
-
-    if(nrow(d1) > 0 & nrow(d2) > 0) {
-
-      p1 <- plot_ly(
-        data = d1,
-        x = ~ group,
-        y = ~ value,
-        color = ~ group,
-        type = "box",
-        text = ~ text,
-        hoverinfo = 'text',
-        colors = c("#BBBDC0", "#287BA5"),
-        boxpoints = ifelse(showJitter,"all","none"),
-        jitter = 1,
-        pointpos = 0
-      ) %>%
-        layout(
-          showlegend = FALSE,
-          xaxis = x,
-          yaxis = y,
-          font = f,
-          margin = margin,
+    p <- plot_ly(
+      data = .data,
+      x = ~ group,
+      y = ~ value,
+      legendgroup = ~ legendGroup,
+      color = ~ legendGroup,
+      type = "box",
+      text = ~ text,
+      hoverinfo = 'text',
+      colors = c("#BBBDC0", "#287BA5"),
+      boxpoints = ifelse(showJitter,"all","none"),
+      pointpos = 0
+    ) %>%
+      layout(
+        showlegend = TRUE,
+        boxmode = "group",
+        title = list(
+          x = 0.05,
           font=list(
             family="Noto Serif', serif",
-            size=30,
-            color="#004F80"
+            size= 24,
+            color="rgb(0, 79, 128)"
           )
+        ),
+        xaxis = x,
+        yaxis = y,
+        font = f,
+        margin = margin,
+        font=list(
+          family="Noto Serif', serif",
+          size=30,
+          color="#004F80"
         )
+      )
 
-      p2 <- plot_ly(
-        data = d2,
-        x = ~ group,
-        y = ~ value,
-        color = ~ group,
-        type = "box",
-        text = ~ text,
-        hoverinfo = 'text',
-        colors = c("#BBBDC0", "#287BA5"),
-        boxpoints = ifelse(showJitter,"all","none"),
-        jitter = 1,
-        pointpos = 0
-      ) %>%
-        layout(
-          showlegend = FALSE,
-          xaxis = x,
-          yaxis = y,
-          font = f,
-          margin = margin,
-          font=list(
-            family="Noto Serif', serif",
-            size=30,
-            color="#004F80"
-          )
-        )
+    p$x$source <- plotName
 
-      p <- subplot(p1, p2, titleX = TRUE) %>%
-        layout(
-          xaxis = list(title = baselineLabel,fixedrange = TRUE), xaxis2 = list(title = comparisonLabel,fixedrange = TRUE),
-          yaxis = list(title = yVariableLabel, fixedrange = TRUE), yaxis2 = list(title="", fixedrange = TRUE)
-        )
-
-      p$x$source <- plotName
-
-      return(p)
-
-    }
-
-    else {
-
-      return(NULL)
-
-    }
+    return(p)
 
   }
 
