b0bd6018d79909b3ddc6f750cbac7c4cd25391ff
Bartsch <kyle.bartsch@cuanschutz.edu>
2020-10-09 09:59:52 -0600

    Added p-value adjustment method parameter to add "(adj)" to labels when adjusted.

--- R/formatPValue.R
+++ R/formatPValue.R
@@ -1,14 +1,26 @@
 #' HTML formats p value based on threshold
 #'
-#' @param pvalue - numeric - p value
-#' @param threshold - numeric - threshold under which to display formatted p value - defaults to 0.05. For values above threshold, returns "No Significant Difference"
-#' @return dataframe with html formatted p value.
+#' @param p.value - numeric - p value
+#' @param p.value.adjustment.method string - p value adjustment method if any - defaults to "none"
+#' @param p.value.threshold - numeric - threshold under which to display formatted p value - defaults to 0.05. For values above threshold, returns "No Significant Difference"
+#' @return string with html formatted p value.
 #' @export
-formatPValue <- function(pvalue,threshold = 0.05){
-  if(pvalue<= threshold){
-    pVal <- formatC(pvalue, format = "e", digits = 2)
-    return(paste0('<p> *p = ',pVal,'</p>'))
-  } else {
-    return(paste0('<p>No significant difference</p>'))
+formatPValue <- function(p.value,p.value.adjustment.method="none",p.value.threshold = 0.05){
+
+  if(p.value <= p.value.threshold){
+
+    p.value <- formatC(p.value, format = "e", digits = 2)
+
+    p.value.suffix <- ifelse(p.value.adjustment.method=="none",""," (adj)")
+
+    return(paste0("<p> *p-value",p.value.suffix," = ",p.value,"</p>"))
+
   }
+
+  else {
+
+    return(paste0('<p>No significant difference</p>'))
+
+  }
+
 }
