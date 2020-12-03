2fb75a47920dcb684d9fcaada8e754e7b1216b77
Bartsch <kyle.bartsch@cuanschutz.edu>
2020-12-03 15:34:40 -0700

    removed threshold paramter. Threshold is inferred based on adjustment method.

--- R/formatPValue.R
+++ R/formatPValue.R
@@ -2,26 +2,27 @@
 #'
 #' @param p.value - numeric - p value
 #' @param p.value.adjustment.method string - p value adjustment method if any - defaults to "none"
-#' @param p.value.threshold - numeric - threshold under which to display formatted p value - defaults to 0.05. For values above threshold, returns "No Significant Difference"
 #' @return string - formatted p value.
 #' @export
-formatPValue <- function(p.value,p.value.adjustment.method="none",p.value.threshold = 0.05){
+formatPValue <- function(p.value,p.value.adjustment.method="none"){
 
-  if(!is.na(p.value)) {
+  if (!is.na(p.value)) {
 
-    if(p.value <= p.value.threshold){
+    pValueAdjustInd <- p.value.adjustment.method != "none"
+    p.value.threshold <- ifelse(pValueAdjustInd,0.1,0.05)
+
+    if (p.value <= p.value.threshold) {
 
       p.value <- formatC(p.value, format = "e", digits = 2)
 
-      p.value.suffix <- ifelse(p.value.adjustment.method=="none",""," (adj)")
+      p.value.suffix <- ifelse(pValueAdjustInd," (adj)","")
 
-      return(paste0("*p-value",p.value.suffix," = ",p.value,""))
-
+      return(paste0("*p-value", p.value.suffix, " = ", p.value, ""))
     }
 
     else {
 
-      return(paste0('No significant difference'))
+      return(paste0("No significant difference"))
 
     }
 
