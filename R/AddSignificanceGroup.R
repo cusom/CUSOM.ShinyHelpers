fbba45acba22e914a1ee45985b48af6af0badfb7
Bartsch <kyle.bartsch@cuanschutz.edu>
2020-12-03 13:05:13 -0700

    Updated naming convention to camel case. Updated function to use threshold parameter as is rather than assuming it should be -log10 transformed.

--- R/AddSignificanceGroup.R
+++ R/AddSignificanceGroup.R
@@ -1,20 +1,20 @@
 #' Function to add 3 group significance group label to fold change dataframe
 #'
 #' @param .data fold change dataframe (x = fold change + y = p value )
-#' @param .foldChangeVar name of fold change column
-#' @param .pValueVar name of p value column
-#' @param .threshold p value threshold defaults to 0.05
+#' @param foldChangeVar name of fold change column
+#' @param pValueVar name of p value column
+#' @param threshold p value threshold defaults to 0.05
 #' @return returns labels for signficance groups (3 - up, down, not significant)
 #' @export
-AddSignificanceGroup <- function(.data, .foldChangeVar, .pValueVar, .threshold=0.05) {
+addSignificanceGroup <- function(.data, foldChangeVar, pValueVar, threshold) {
 
-  .foldChangeVar <- enquo(.foldChangeVar)
-  .pValueVar <- enquo(.pValueVar)
+  foldChangeVar <- enquo(foldChangeVar)
+  pValueVar <- enquo(pValueVar)
 
   .data %>%
     mutate(significanceGroup = case_when(
-      (!!.pValueVar > -log10(.threshold) & !!.foldChangeVar < - 0 ) ~ 'Significant (down)',
-      (!!.pValueVar > -log10(.threshold) & !!.foldChangeVar >=  0 ) ~ 'Significant (up)'
+      (!!pValueVar > threshold & !!foldChangeVar < - 0 ) ~ 'Significant (down)',
+      (!!pValueVar > threshold & !!foldChangeVar >=  0 ) ~ 'Significant (up)'
     )) %>%
     replace_na(list(significanceGroup ="Not Significant"))
 
