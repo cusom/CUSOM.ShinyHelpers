d59095e0bce162d4a9d852b821d9af1539ec8c18
Kyle Bartsch <kyle.bartsch@ucdenver.edu>
2021-04-12 16:58:06 -0600

    Formatted string now returns "q-value" when adjustment method is used, "p-value" otherwise.

--- R/formatPValue.R
+++ R/formatPValue.R
@@ -2,7 +2,7 @@
 #'
 #' @param p.value - numeric - p value
 #' @param p.value.adjustment.method string - p value adjustment method if any - defaults to "none"
-#' @return string - formatted p value.
+#' @return string - formatted p or q value depending on adjustment method.
 #' @export
 formatPValue <- function(p.value,p.value.adjustment.method="none"){
 
@@ -14,10 +14,8 @@ formatPValue <- function(p.value,p.value.adjustment.method="none"){
     if (p.value <= p.value.threshold) {
 
       p.value <- formatC(p.value, format = "e", digits = 2)
-
-      p.value.suffix <- ifelse(pValueAdjustInd," (adj)","")
-
-      return(paste0("*p-value", p.value.suffix, " = ", p.value, ""))
+      p.value.prefix <- ifelse(pValueAdjustInd,"q","p")
+      return(paste0(p.value.prefix, "-value = ", p.value))
     }
 
     else {
