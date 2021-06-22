158eff604ef0320291af7dc73fdc9542569ff905
Kyle Bartsch <kyle.bartsch@ucdenver.edu>
2021-06-22 14:07:05 -0600

    adding helper functions to execute DBI odbc database queries

--- NEWS.md
+++ NEWS.md
@@ -1,4 +1,4 @@
-# CUSOMShinyHelpers 1.4.1
+# CUSOMShinyHelpers 1.4.2
  Adding new functions to support correlation modules
  
 ## Major Changes 
@@ -6,6 +6,7 @@
 - getDensityColors - function to get density colors for scatter plot
 - getScatterPlotWithSmoothing - function to generate ggplot scatter plot with smoothing method (lm, loess, etc)
 - Updating getVolcanoPlot to accept optional color and shape parameters - allows for control over colors used in volcano and changing specific shapes used within volcano plot. 
+- added helper functions to execute DBI odbc database connections  
  
 ## Minor Changes
 -adding small helper function to parse delimited strings
---
+++ R/databaseQueryUtilities.R
@@ -0,0 +1,50 @@
+#' Function to pull data from database using paramterized DBI odbc query
+#' @param queryString string paramterized sql query where parameters are indicated by "?"
+#' @param paramters tibble of parameter values
+#' @param converFactorsToStrings boolean whether to convert all factor variables to character strings default = TRUE
+#' @param ... conn_args list of database connection arguments: driver, server, database name, userid, password, port
+#' @return returns sql query result as a dataframe
+#' @export
+getDataframeFromDatabase <- function(queryString,parameters,convertFactorsToStrings = TRUE,...) {
+
+  if(!is.null(parameters)){
+    parameters %>%
+      mutate_if(is.factor, as.character) -> parameters
+  }
+
+  dbhandle <- dbConnector(...)
+
+  query <- dbSendQuery(dbhandle,queryString)
+
+  dbBind(query, parameters)
+
+  data <- dbFetch(query)
+
+  dbClearResult(query)
+
+  if(convertFactorsToStrings){
+    data <- data %>%
+      mutate_if(is.factor, as.character) -> data
+  }
+
+  return(data)
+
+}
+
+#' Function to create DBI odbc database connection object
+#' @param conn_args list of database connection arguments: driver, server, database name, userid, password, port
+#' @return returns DBI odbc database connection object
+dbConnector <- function(conn_args) {
+
+  con <- DBI::dbConnect(odbc::odbc(),
+                        Driver   = conn_args$driver,
+                        Server   = conn_args$server,
+                        Database = conn_args$database,
+                        UID      = conn_args$uid,
+                        PWD      = conn_args$pwd,
+                        Port     = conn_args$port
+  )
+
+  return(con)
+
+}
